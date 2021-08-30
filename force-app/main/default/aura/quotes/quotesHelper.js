5be5c9502793d22b1ae535a1cee9d6bb3aff0bd8
DanyYanez <dany2332@hotmail.es>
2021-08-30 16:25:20 -0400

    commit

--- force-app/main/default/aura/quotes/quotesHelper.js
+++ force-app/main/default/aura/quotes/quotesHelper.js
@@ -3,11 +3,11 @@
 		let bandName = component.get('v.bandName');
         let bandQuote;
         if(bandName == 'BTS'){
-            bandQuote = "Don't be trapped in someone else's dream.";
+            bandQuote = "\"Don't be trapped in someone else's dream.\"";
         } else if(bandName == 'Blackpink'){
-            bandQuote = "All eyes on me when I step in the room.";
+            bandQuote = "\"All eyes on me when I step in the room.\"";
         } else {
-            bandQuote = "Put away all other thoughts, do whatever you want";
+            bandQuote = "\"Put away all other thoughts, do whatever you want\"";
         }
         component.set('v.quote', bandQuote);
 	}
